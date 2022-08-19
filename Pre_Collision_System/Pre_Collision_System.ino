6f7d8c95259fdc2572c890b7c6701f4762ab7056
makerlabvn <40706910+makerlabvn@users.noreply.github.com>
2022-08-19 17:22:24 +0700

    Add files via upload

--- Pre_Collision_System/Pre_Collision_System.ino
+++ Pre_Collision_System/Pre_Collision_System.ino
@@ -3,6 +3,15 @@
 #include "UltraSonicDistanceSensor.h"
 #include "LiquidCrystal_I2C.h"
 
+// Define connected sensor pin
+// Dat ten cong ket noi cho cam bien
+#define  LED_R_PIN    A1
+#define  LED_Y_PIN    A2
+#define  LED_G_PIN    A3
+#define  BUZZER_PIN   9
+#define  TRIG_PIN     13
+#define  ECHO_PIN     12
+
 // Declare variables 
 // Khai bao bien
 int distance_value;
@@ -10,7 +19,7 @@ int Range_Danger;
 int Range_Warning;
 int Range_Close_2;
 int Range_Safe;
-UltraSonicDistanceSensor distanceSensor(13, 12);
+UltraSonicDistanceSensor distanceSensor(TRIG_PIN, ECHO_PIN);
 
 LiquidCrystal_I2C LCD(39, 20, 04);
 
@@ -21,10 +30,10 @@ void setup() {
   // Turn on LCD backlight   
   // Bat den nen LCD 1602
   LCD.backlight();
-  pinMode(A1, OUTPUT);
-  pinMode(A2, OUTPUT);
-  pinMode(A3, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(LED_R_PIN, OUTPUT);
+  pinMode(LED_Y_PIN, OUTPUT);
+  pinMode(LED_G_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
   Range_Safe = 45;
   Range_Warning = 35;
   Range_Danger = 15;
@@ -44,44 +53,44 @@ void loop() {
     LCD.setCursor(2, 0);
     LCD.print("Safe ^^      ");
     // red
-    digitalWrite(A1, 0);
+    digitalWrite(LED_R_PIN, 0);
     // yellow
-    digitalWrite(A2, 0);
+    digitalWrite(LED_Y_PIN, 0);
     // green
-    digitalWrite(A3, 0);
+    digitalWrite(LED_G_PIN, 0);
     // buzzer
-    digitalWrite(9, 0);
+    digitalWrite(BUZZER_PIN, 0);
   } else {
     if ((distance_value < Range_Safe)  &&  (distance_value > Range_Warning)) {
       LCD.setCursor(2, 0);
       LCD.print("Warning :(   ");
-      digitalWrite(A1, 0);
-      digitalWrite(A2, 0);
-      digitalWrite(A3, 1);
-      digitalWrite(9, 1);
+      digitalWrite(LED_R_PIN, 0);
+      digitalWrite(LED_Y_PIN, 0);
+      digitalWrite(LED_G_PIN, 1);
+      digitalWrite(BUZZER_PIN, 1);
       delay(300);
-      digitalWrite(A3, 0);
-      digitalWrite(9, 0);
+      digitalWrite(LED_G_PIN, 0);
+      digitalWrite(BUZZER_PIN, 0);
     }
     if ((distance_value < Range_Warning)  &&  (distance_value > Range_Danger)) {
       LCD.setCursor(2, 0);
       LCD.print("Dangerous >.<");
-      digitalWrite(A1, 0);
-      digitalWrite(A2, 1);
-      digitalWrite(A3, 1);
-      digitalWrite(9, 1);
+      digitalWrite(LED_R_PIN, 0);
+      digitalWrite(LED_Y_PIN, 1);
+      digitalWrite(LED_G_PIN, 1);
+      digitalWrite(BUZZER_PIN, 1);
       delay(100);
-      digitalWrite(A2, 0);
-      digitalWrite(A3, 0);
-      digitalWrite(9, 0);
+      digitalWrite(LED_Y_PIN, 0);
+      digitalWrite(LED_G_PIN, 0);
+      digitalWrite(BUZZER_PIN, 0);
     }
     if (distance_value < Range_Danger) {
       LCD.setCursor(2, 0);
       LCD.print("Crash T.T    ");
-      digitalWrite(A1, 1);
-      digitalWrite(A2, 1);
-      digitalWrite(A3, 1);
-      digitalWrite(9, 1);
+      digitalWrite(LED_R_PIN, 1);
+      digitalWrite(LED_Y_PIN, 1);
+      digitalWrite(LED_G_PIN, 1);
+      digitalWrite(BUZZER_PIN, 1);
     }
   }
 }
